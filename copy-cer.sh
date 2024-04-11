2c0a57ac435565d674de683def826236bcdad66c
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2024-04-12 03:40:37 +0900

    コンテナIDから取得するように修正

--- copy-cer.sh
+++ copy-cer.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker cp smtptest_smtp4dev_1:./smtp4dev/selfsigned-certificate.cer ./ssl/
+# Docker Compose ps の出力からコンテナIDを取得
+CONTAINER_ID=$(docker-compose -p $(basename "$(pwd)") ps -q)
+
+# ローカルに証明書をコピー
+docker cp $CONTAINER_ID:./smtp4dev/selfsigned-certificate.cer ./ssl/
